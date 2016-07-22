e2043c29db62763c8ebc0e9697a822268e8ee2f3
Alfred <alfred.francis@pearldatadirect.com>
2016-07-22 12:35:32 +0530

    added redcurrant live api

--- ikyActions/actions.py
+++ ikyActions/actions.py
@@ -7,29 +7,30 @@ from datetime import datetime, timedelta
 
 def hello(entities):
     return "hello"
-
-
 def checkH2HTransactionStatus(entities):
-    url = "http://172.30.10.119:8094/callyourpartner/YOM/%s" % (entities["routingKey"])
-    data = {
-        "txnType": "2O",
-        "method": "enquiryTxn",
-        "txnRefNum": entities["txnNo"],
-        "serviceProviderCode": "LULUXAE#####",
-        "uploadDate": "20-07-2016"
-    }
+    if not entities["routingKey"]:
+        return "Routing key not supported."
+    url = "http://172.30.10.119:8094/callyourpartner/YOM/%s"%entities["routingKey"]
+    data = {"payload": json.dumps({
+        "txnType":"2O",
+        "method":"enquiryTxn",
+        "txnRefNum":entities["txnNo"]}
+    )}
     try:
-        response = requests.get(url, data)
-        json_dict = json.loads(response.content)
-        if "statusDescription" not in json_dict:
+        response = requests.post(url, data=data)
+        responseDict = json.loads(response.text)
+        if "statusDescription" not in responseDict:
             return "Status not available in Red currant."
         else:
-            json_dict["statusDescription"]
+            return responseDict["statusDescription"]
     except:
-        return "Red currant Not avilable"
+        return "Redcurrant Server Not avilable"
 
 
 def checkTransactionStatus(entities):
+    routingKeyMatchTable = {
+        11667: "DIBHH"
+    }
     url = "http://172.30.10.119:7027/rateboard/enquiry/10/146281095932945"
     parameters = {"txnno": entities['txnNo']}
     if not entities['txnNo']:
@@ -38,14 +39,19 @@ def checkTransactionStatus(entities):
         response = requests.get(url, params=parameters)
         responseDict = json.loads(response.text)
         statusDesc = responseDict.get('statusDesc')
+        rcResult=yomResult = "Not Avilable"
         if not (statusDesc):
-            result = "Status not available in YOM"
+            yomResult = "Status not available in YOM"
+        elif "Transmitted" in statusDesc:
+            yomResult = statusDesc
+            entities["routingKey"] = routingKeyMatchTable.get(responseDict.get('beneficiary').get('beneficiaryBank').get('draweeBankId'))
+            rcResult = checkH2HTransactionStatus(entities)
         else:
-            result = statusDesc
-        return ("Transaction no :%s, %s") % (entities['txnNo'], result)
+            yomResult = statusDesc
+        return ("***Transaction no :%s ***YOM status :%s ***RedCurrant status: %s")%(entities['txnNo'],
+                                                                            yomResult,rcResult)
     except Exception as e:
         return "Server Error,Please try again later."
-
 
 def calculateOutTime(entities):
     userId = "356000199"
