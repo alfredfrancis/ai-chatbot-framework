262b24aebe22bf7b19e3db92454f9a43684ea768
Michael Luk <michaelluk503@gmail.com>
2017-02-28 12:50:48 +0800

    Download missing nltk datasets in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 import nltk
 import os
 
+# Downloading necessary NLTK datasets
+nltk.download("stopwords")
+nltk.download("wordnet")
+nltk.download('averaged_perceptron_tagger')
+nltk.download('punkt')
+
 #creating directory for storing chat logs
 if not os.path.exists("logs"):
     os.makedirs("logs")
