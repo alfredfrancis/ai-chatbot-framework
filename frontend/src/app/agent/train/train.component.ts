354e51a5bde13271127c0d7eeb18fff4fbe1aa57
Alfred francis <alfredfranciz@gmail.com>
2018-05-13 16:56:00 +0530

    bugfix contenteditable auto save

--- frontend/src/app/agent/train/train.component.ts
+++ frontend/src/app/agent/train/train.component.ts
@@ -203,4 +203,24 @@ export class TrainComponent implements OnInit {
         }
     }
 }
+
+//place curser at the end of content editable div
+  placeCaretAtEnd(el) {
+    el.focus();
+    if (typeof window.getSelection != "undefined"
+            && typeof document.createRange != "undefined") {
+        var range = document.createRange();
+        range.selectNodeContents(el);
+        range.collapse(false);
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } else if (typeof document.body.createTextRange != "undefined") {
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(el);
+        textRange.collapse(false);
+        textRange.select();
+    }
+  }
+
 }
