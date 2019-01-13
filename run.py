5c7b0b18d904b6100cc8c446abb82b4213df57fc
tnodine <3662907+tnodine@users.noreply.github.com>
2019-01-12 23:41:29 -0600

    NEVER expose a flask server with debug=True over the internet. Especially by default. (#122)

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from app import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
+    app.run(host='127.0.0.1', port=8080, debug=True, threaded=True)
