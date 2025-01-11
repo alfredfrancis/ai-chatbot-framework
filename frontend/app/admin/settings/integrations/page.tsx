e43fc1338038cc7b404300a1c8554ecaa76ea00c
Alfred Francis <alfredfranciz@gmail.com>
2025-01-11 09:01:42 +0100

    host chat widget in gh pages

--- frontend/app/admin/settings/integrations/page.tsx
+++ frontend/app/admin/settings/integrations/page.tsx
@@ -8,7 +8,7 @@ const IntegrationsPage: React.FC = () => {
 
   const widgetCode = `<script type="text/javascript">
 !function(win,doc){"use strict";var script_loader=()=>{try
-{var head=doc.head||doc.getElementsByTagName("head")[0],script=doc.createElement("script");script.setAttribute("type","text/javascript"),script.setAttribute("src",win.iky_base_url+"/widget/script.js"),head.appendChild(script)}
+{var head=doc.head||doc.getElementsByTagName("head")[0],script=doc.createElement("script");script.setAttribute("type","text/javascript"),script.setAttribute("src","https://alfredfrancis.in/ai-chatbot-framework/app/static/widget/script.js"),head.appendChild(script)}
 catch(e){}};win.chat_context={"username":"John"},win.iky_base_url="${baseUrl}",script_loader()}(window,document);
 </script>`;
 
